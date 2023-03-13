7bf6fb232b2117404e3a0054b593fa32121fce31
Lawrence <nderitunjau@gmail.com>
2023-03-13 15:32:06 +0300

    Splash Changes

--- app/src/main/java/com/gdff/app/MainActivity.kt
+++ app/src/main/java/com/gdff/app/MainActivity.kt
@@ -3,12 +3,20 @@ package com.gdff.app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this@MainActivity, User_registration::class.java))
-        finish()
+        setContentView(R.layout.activity_main)
+        //contructor Handler() seems to have been deprecated,tomorrow we might try a different approach towards this
+        Handler().postDelayed(Runnable { //This method will be executed once the timer is over
+            // Start your app main activity
+            val i = Intent(this@MainActivity, User_registration::class.java)
+            startActivity(i)
+            // close this activity
+            finish()
+        }, 3000)
     }
 }
