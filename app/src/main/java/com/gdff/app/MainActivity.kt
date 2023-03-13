d19feec54240354a6a0722293a17ee7e74bed03c
Lawrence <nderitunjau@gmail.com>
2023-03-13 15:24:41 +0300

    SplashScreen using launcher theme n timer

--- app/src/main/java/com/gdff/app/MainActivity.kt
+++ app/src/main/java/com/gdff/app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.gdff.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
+        startActivity(Intent(this@MainActivity, User_registration::class.java))
+        finish()
     }
 }
