78c170f613064ad7e9e274ec8ec3fc61c8e79730
nderitu_njau <nderitunjau@gmail.com>
2023-03-13 15:49:00 +0300

    Update MainActivity.kt

--- app/src/main/java/com/gdff/app/MainActivity.kt
+++ app/src/main/java/com/gdff/app/MainActivity.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+/*There is no need to setContentView() with any XML layout because the splash screen is being loaded from the manifest already. 
+Create a Handler if you want to show your splash screen for a set period of time, such as five seconds.
+A timer can also let you download data from the cloud on your main activity, which might be a better strategy than displaying a dialogue on the home screen.
+*/
 
 class MainActivity : AppCompatActivity() {
     
